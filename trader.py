df4b60c8d1b16a2c0ba48a29a0e6b222c2e61e51
AyushPatel101 <patayush01@gmail.com>
2023-03-20 13:26:03 -0500

    use max volume

--- trader.py
+++ trader.py
@@ -15,11 +15,14 @@ class Trader:
         # Iterate over all the keys (the available products) contained in the order depths
         for product in state.order_depths.keys():
 
-            # Check if the current product is the 'PEARLS' product, only then run the order logic
+            # Check if the current product is the 'Ba' product, only then run the order logic
             if product == 'BANANAS':
 
+                max_buy = 20 - state.position[product]
+                max_sell = -20 - state.position[product]
                 # Retrieve the Order Depth containing all the market BUY and SELL orders for PEARLS
                 order_depth: OrderDepth = state.order_depths[product]
+
 
                 # Initialize the list of Orders to be sent as an empty list
                 orders: list[Order] = []
@@ -29,16 +32,18 @@ class Trader:
                 acceptable_price = 1
 
                 # If statement checks if there are any SELL orders in the PEARLS market
-                if len(order_depth.sell_orders) > 0:
+                sell_keys_lst = sorted(order_depth.sell_orders.keys())
+                for best_ask in sell_keys_lst:
 
                     # Sort all the available sell orders by their price,
                     # and select only the sell order with the lowest price
-                    best_ask = min(order_depth.sell_orders.keys())
-                    best_ask_volume = order_depth.sell_orders[best_ask]
-
+                    #best_ask = min(order_depth.sell_orders.keys())
+                
                     # Check if the lowest ask (sell order) is lower than the above defined fair value
                     if best_ask < acceptable_price:
-
+                        best_ask_volume = order_depth.sell_orders[best_ask]
+                        vol_to_trade = min(best_ask_volume, max_buy)
+                        max_buy -= vol_to_trade
                         # In case the lowest ask is lower than our fair value,
                         # This presents an opportunity for us to buy cheaply
                         # The code below therefore sends a BUY order at the price level of the ask,
@@ -46,18 +51,30 @@ class Trader:
                         # We expect this order to trade with the sell order
                         print("BUY", str(-best_ask_volume) + "x", best_ask)
                         orders.append(Order(product, best_ask, -best_ask_volume))
+                    else: 
+                        break
+                    if max_buy <= 0:
+                        break
 
                 # The below code block is similar to the one above,
                 # the difference is that it finds the highest bid (buy order)
                 # If the price of the order is higher than the fair value
-                # This is an opportunity to sell at a premium
-                if len(order_depth.buy_orders) != 0:
-                    best_bid = max(order_depth.buy_orders.keys())
-                    best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        print("SELL", str(best_bid_volume) + "x", best_bid)
-                        orders.append(Order(product, best_bid, -best_bid_volume))
+                # This is an opportunity to sell at a premium   
+                buy_keys_lst = sorted(order_depth.buy_orders.keys(), reverse = True)
+                for best_bid in buy_keys_lst:
 
+                    # best_bid = max(order_depth.buy_orders.keys())
+
+                    if best_bid > acceptable_price:
+                        best_bid_volume = order_depth.buy_orders[best_bid]
+                        vol_to_trade = min(best_bid_volume, max_sell)
+                        max_sell -= vol_to_trade
+                        print("SELL", str(vol_to_trade) + "x", best_bid)
+                        orders.append(Order(product, best_bid, -vol_to_trade))
+                    else:
+                        break
+                    if max_sell <= 0:
+                        break
                 # Add all the above orders to the result dict
                 result[product] = orders
 
