a70dec63d4144dc121f9425bb56dca8edfa95cd8
AyushPatel101 <patayush01@gmail.com>
2023-03-21 01:16:23 -0500

    twrewa

--- trader.py
+++ trader.py
@@ -9,6 +9,8 @@ class Trader:
         Only method required. It takes all buy and sell orders for all symbols as an input,
         and outputs a list of orders to be sent
         """
+
+        #
         # Initialize the method output dict as an empty dict
         result = {}
         # Iterate over all the keys (the available products) contained in the order depths
